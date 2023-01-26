301e5d4a84776adeaef8c05f56c532c7c6e4598d
Victor Wu <victorwu@victors-air.nunet.neu.edu>
2023-01-26 15:58:28 -0800

    add three buttons to Confirm

--- screens/Confirm.js
+++ screens/Confirm.js
@@ -1,16 +1,38 @@
-import { View, Text, Modal, StyleSheet } from 'react-native'
-import React from 'react'
-import Card from '../components/Card'
+import { View, Text, Button } from "react-native";
+import React from "react";
+import Card from "../components/Card";
 
-export default function Confirm({styles, changePage, email, phone}) {
+export default function Confirm({ styles, changePage, email, phone }) {
   return (
     <View style={styles.container}>
       <Card>
-        <Text style = {styles.largeText}>You have entered:</Text>
-        <Text style = {styles.largeText}>{email}</Text>
-        <Text style = {styles.largeText}>{phone}</Text>
-        <Text style = {styles.largeText}>Please confirm they are correct.</Text>
+        <Text style={styles.largeText}>You have entered:</Text>
+        <Text style={styles.largeText}>{email}</Text>
+        <Text style={styles.largeText}>{phone}</Text>
+        <Text style={styles.largeText}>Please confirm they are correct.</Text>
+        <View style={{ padding: 5 }}>
+          <Button
+            onPress={() => goBack()}
+            title="Go Back"
+            color="red"
+          />
+        </View>
+        <View style={{ padding: 5 }}>
+          <Button
+            onPress={() => confirm()}
+            title="Confirm"
+            color="blue"
+          />
+        </View>
+
+        <View style={{ padding: 5 }}>
+          <Button
+            onPress={() => finishLater()}
+            title="Finish Later"
+            color="blue"
+          />
+        </View>
       </Card>
     </View>
-  )
+  );
 }
